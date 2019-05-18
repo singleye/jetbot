c001345a85dd644d9c527207b22afb2cca93a80a
singleye <singleye@163.com>
2019-05-18 22:44:48 +0800

    Add object detection

---
+++ nano/camera.py
@@ -0,0 +1,75 @@
+import traitlets
+from traitlets.config.configurable import SingletonConfigurable
+import atexit
+import cv2
+import threading
+import numpy as np
+
+
+class Camera(SingletonConfigurable):
+    
+    value = traitlets.Any()
+    detected = traitlets.Any()
+    
+    # config
+    width = traitlets.Integer(default_value=224).tag(config=True)
+    height = traitlets.Integer(default_value=224).tag(config=True)
+    fps = traitlets.Integer(default_value=21).tag(config=True)
+    capture_width = traitlets.Integer(default_value=3280).tag(config=True)
+    capture_height = traitlets.Integer(default_value=2464).tag(config=True)
+
+    def __init__(self, *args, **kwargs):
+        self._running = False
+        self.value = np.empty((self.height, self.width, 3), dtype=np.uint8)
+        self.detected = np.empty((self.height, self.width, 3), dtype=np.uint8)
+        super(Camera, self).__init__(*args, **kwargs)
+
+        try:
+            self.cap = cv2.VideoCapture(self._gst_str(), cv2.CAP_GSTREAMER)
+
+            re, image = self.cap.read()
+
+            if not re:
+                raise RuntimeError('Could not read image from camera.')
+
+            self.value = image
+            self.start()
+        except:
+            self.stop()
+            raise RuntimeError(
+                'Could not initialize camera.  Please see error trace.')
+
+        atexit.register(self.stop)
+
+    def _capture_frames(self):
+        while True:
+            re, image = self.cap.read()
+            if re:
+                self.value = image
+            else:
+                break
+                
+    def _gst_str(self):
+        return 'v4l2src device="/dev/video0" ! videoconvert ! appsink'
+    
+    def start(self):
+        if not self.cap.isOpened():
+            self.cap.open(self._gst_str(), cv2.CAP_GSTREAMER)
+        if not hasattr(self, 'thread') or not self.thread.isAlive():
+            self.thread = threading.Thread(target=self._capture_frames)
+            self.thread.start()
+        self._running = True
+
+    def stop(self):
+        self._running = False
+        if hasattr(self, 'cap'):
+            self.cap.release()
+        if hasattr(self, 'thread'):
+            self.thread.join()
+            
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def running(self):
+        return self._running
